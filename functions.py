4264d4e3a8d14c49a388bc684c2bc6547fc03177
niccobal <niccolo.baldelli@gmail.com>
2019-03-28 16:04:23 +0100

    Added functions for creation of vortices in the order parameter

--- functions.py
+++ functions.py
@@ -60,29 +60,7 @@ def sprshamPBC(L,t,mu,phase): #DANGER mu sign mst be opposite of t
             matr.appendx(i,nearn[j],-t*np.exp(phase[j]))    
     return sprs.coo_matrix((matr.data,(matr.row,matr.col)))
 
-def sprspy(L): #py operator matrix (already multiplied by -i)
-    matr=cl.matrixdata()
-    for i in range(L**2):
-        matr.appendx(i,i,0)
-        nearn=nearneighOBC(i,L)
-        if (nearn[3]!=-1):    
-            matr.appendx(i,nearn[3],-1j)
-        if (nearn[1]!=-1):    
-            matr.appendx(i,nearn[1],1j)
-    return sprs.coo_matrix((matr.data,(matr.row,matr.col)))
-
-def sprspx(L): #px operator matrix (already multiplied by -i)
-    matr=cl.matrixdata()
-    for i in range(L**2):
-        matr.appendx(i,i,0)
-        nearn=nearneighOBC(i,L)
-        if (nearn[0]!=-1):
-            matr.appendx(i,nearn[0],-1j) 
-        if (nearn[2]!=-1):            
-            matr.appendx(i,nearn[2],1j)
-    return sprs.coo_matrix((matr.data,(matr.row,matr.col)))
-
-def noncons(L,delta):
+def nonconsPBC(L,delta):
     matr=cl.matrixdata()
     for i in range(L**2):
         matr.appendx(i,i,0)
@@ -92,15 +70,84 @@ def noncons(L,delta):
             matr.appendx(i,nearn[j],coupl[j])
     return sprs.coo_matrix((matr.data,(matr.row,matr.col)))
         
-def expvalue(a,vec):
+def nonconsOBC(L,delta):
+    matr=cl.matrixdata()
+    for i in range(L**2):
+        matr.appendx(i,i,0)
+        nearn=nearneighOBC(i,L)
+        coupl=np.array([delta,-1j*delta,-delta,1j*delta])
+        for j in range(len(nearn)): 
+            if (nearn[j]==-1):
+                continue
+            matr.appendx(i,nearn[j],coupl[j])
+    return sprs.coo_matrix((matr.data,(matr.row,matr.col)))  
+
+def phaser(i,vortex,Ltilde):
+    asc,ordin=indtocord(i,Ltilde)
+    if (asc==vortex[0])&(ordin==vortex[1]):
+        return 0
+    return ((ordin-vortex[0])+1j*(vortex[1]-asc))/(np.sqrt((asc-vortex[0])**2+(ordin-vortex[1])**2))
+
+def nonconsPBCvortex(L,delta,vortex):
+    matr=cl.matrixdata()
+    for i in range(L**2):
+        matr.appendx(i,i,0)
+        nearn=nearneighPBC(i,L)
+        kcoup=np.array([delta,-1j*delta,-delta,1j*delta])
+        for j in range(len(nearn)):
+            coup=np.sqrt(phaser(i,vortex,L)*phaser(nearn[j],vortex,L))
+            matr.appendx(i,nearn[j],kcoup[j]*coup)
+    return sprs.coo_matrix((matr.data,(matr.row,matr.col)))
+
+def nonconsOBCvortex(L,delta,vortex):
+    matr=cl.matrixdata()
+    for i in range(L**2):
+        matr.appendx(i,i,0)
+        nearn=nearneighOBC(i,L)
+        kcoup=np.array([delta,-1j*delta,-delta,1j*delta])
+        for j in range(len(nearn)): 
+            if (nearn[j]==-1):
+                continue
+            coup=np.sqrt(phaser(i,vortex,L)*phaser(nearn[j],vortex,L))
+            matr.appendx(i,nearn[j],kcoup[j]*coup)
+    return sprs.coo_matrix((matr.data,(matr.row,matr.col)))
+
+def number(vecs,Ltilde):
+    num=0
+    for i in range(Ltilde**2):
+        num+=np.conj(vecs[Ltilde**2:,i])@vecs[Ltilde**2:,i]
+    return num
+
+def density(asc,ordin,vecs,Ltilde):
+    i=cordtoind(asc,ordin,Ltilde)
+    return sum(np.conj(vecs[i,Ltilde**2:])*vecs[i,Ltilde**2:])
+
+def densityplot(asc,ordin,vecs,Ltilde):
+    n=np.zeros((Ltilde,Ltilde))
+    for i in range(Ltilde):
+        for j in range(Ltilde):
+            n[i][j]=density(asc[i][j],ordin[i][j],vecs,Ltilde)
+    return n
+
+def expvalue(a,vec): #expectation value of operators
     exp=np.conj(vec)@a@vec
     return exp
 
-def en(asc,ordin,t,a,mu): # scalable tight binding 2D energy
+def en(asc,ordin,t,a,mu): #scalable tight binding 2D energy
     return -2*t*a**-2*(np.cos(asc*a)+np.cos(ordin*a)-2)-mu
 
-def enbog(kx,ky,t,a,mu,delta):
+def enbog(kx,ky,t,a,mu,delta): #bogoliubov dispersion in discretized space with spacing a
     return np.sqrt((en(kx,ky,t,a,mu))**2+(delta/a)**2*((np.sin(kx*a))**2+(np.sin(ky*a))**2))
 
-def enbogcont(kx,ky,t,mu,delta):
+def enbogcont(kx,ky,t,mu,delta): #bogoliubov dispersion in continuum space
     return np.sqrt((t*(kx**2+ky**2)-mu)**2+(delta)**2*(kx**2+ky**2))
+
+def indtocord(i,Ltilde):
+    cord=np.array([0,0])
+    cord[0]=np.floor(i/Ltilde)
+    cord[1]=i%Ltilde
+    return cord
+
+def cordtoind(asc,ordin,Ltilde):
+    return ordin*Ltilde+asc
+
